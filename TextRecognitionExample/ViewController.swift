5e6822ab077d0b9613378acea040715d5d0fcd5b
Paul Wiesinger <paul.n.w@icloud.com>
2019-08-14 09:48:43 +0200

    init

--- TextRecognitionExample/ViewController.swift
+++ TextRecognitionExample/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupImage()
+        doVision()
     }
 
-
+    func setupImage() {
+        let url = Bundle.main.url(forResource: "image", withExtension: ".JPG")
+        let data = try! Data.init(contentsOf: url!)
+        imageView.image = UIImage.init(data: data)
+    }
+    
+    func doVision() {
+        let request = VNRecognizeTextRequest.init { (request, error) in
+            if let error = error {
+                print (error.localizedDescription)
+            }
+            
+            var resultStr = ""
+            
+            for result in (request.results!) as! [VNRecognizedTextObservation] {
+                result.topCandidates(1).forEach { (recognizedText) in
+                    resultStr += recognizedText.string + "\n"
+                    print(recognizedText.string)
+                }
+            }
+            self.label.text = resultStr
+        }
+        
+        request.recognitionLevel = .accurate
+        //request.recognitionLanguages = ["de-de"]
+        
+        let requestHandler = VNImageRequestHandler.init(data: imageView.image!.pngData()!, options: [:])
+        try! requestHandler.perform([request])
+    }
 }
 
