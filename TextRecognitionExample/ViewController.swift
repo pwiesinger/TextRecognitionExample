7b01bb80bf13180cb497458e3ea3c0fd0f841c1d
Paul Wiesinger <paul.n.w@icloud.com>
2019-08-14 09:14:23 +0200

    Initial Commit

---
+++ TextRecognitionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognitionExample
+//
+//  Created by Paul Wiesinger on 14.08.19.
+//  Copyright © 2019 Paul Wiesinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
